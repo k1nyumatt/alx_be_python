1744b3d61bea011924a5876eb87d5c9538c42f40
martin kinyua <kinyuam664@gmail.com>
2025-11-07 04:40:32 +0100

    correction of basic_operations.py inside python_introduction

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,7 +1,8 @@
 number1 = 10
 number2 = 5
-
-print(f"Addition of {number1} and {number2} is {number1 + number2}")
-print(f"Subtraction of {number1} and {number2} is {number1 - number2}")
-print(f"Multiplication of {number1} and {number2} is {number1 * number2}")
-)
+addition = number1 + number2 
+subtraction = number1 - number2
+multiplication = number1 * number2
+print(addition)
+print(subtraction)
+print(multiplication)
