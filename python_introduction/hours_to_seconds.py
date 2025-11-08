fb4703ac1ab583412fc8bf0ecc67bdc9ee0fca09
martin kinyua <kinyuam664@gmail.com>
2025-11-08 03:09:19 +0300

    correction of proper wording inside hours_to_seconds.py

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,8 +1,8 @@
 #convert hours to seconds
-hour = 2
+hours = 2
 seconds_in_an_hour = 3600 # there are 60 minutes in one hour, therefore 60 x 60 to give you seconds in an hour
-seconds = hour * seconds_in_an_hour
-print(hour * seconds_in_an_hour)
+seconds = hours * seconds_in_an_hour
+print(hours * seconds_in_an_hour)
 
 
 
