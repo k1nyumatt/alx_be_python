2782c24b1c21b14329f2d93d417c6d4f5de3fccc
martin kinyua <kinyuam664@gmail.com>
2025-11-08 05:15:45 +0300

    addition of finance_calculator.py into python_introduction

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,9 @@
+monthly_income= int(input("Enter your monthly income:"))
+monthly_expenses= int(input("Enter your total monthly expenses:"))
+monthly_savings= monthly_income - monthly_expenses
+print(monthly_savings)
+projected_savings= (1000 * 12 + (1000 *12 * 0.05))
+print(monthly_savings)
+print(projected_savings)
+
+
